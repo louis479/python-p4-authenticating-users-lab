bb5d90aeadcf2945bb581772c9614e389560a161
louis-ngugi <louisngugi858@gmail.com>
2025-04-17 05:02:07 +0300

    learnt authentication

--- server/app.py
+++ server/app.py
@@ -18,6 +18,37 @@ db.init_app(app)
 
 api = Api(app)
 
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+
+        else:
+            return {"error": "Unauthorized"}, 401  # Still JSON
+        
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+
+        if user_id:
+            user = User.query.get(user_id)
+            return user.to_dict(), 200
+
+        return {}, 401
+
+
 class ClearSession(Resource):
 
     def delete(self):
@@ -51,6 +82,10 @@ class ShowArticle(Resource):
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
+
 
 
 if __name__ == '__main__':
